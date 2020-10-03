9777a901e29ac8eb5a7301b7a37b2895e57bec5c
georjino <52850501+georjino@users.noreply.github.com>
2020-10-03 20:22:49 +0300

    Modifying db access key

--- headers.js
+++ headers.js
@@ -3,7 +3,7 @@ import CryptoJS from 'crypto-js';
 export const AzureToken = (url,method) => {
 
     //const mastKey = "gLNLj4yNpqWGd6LUFGU7PuHcG7h3EIctW500HqfZt4nTXPCSxf7KA3YZ5wnsBfXIpq1S3sH3E4V6L3A2vQX7RQ==";
-    const mastKey = "vuz6NKtp2hX5YfMsRXSmiy1UKlDREu7FKX9VTymCRExivgQwyk3plFIxuOH5FAGFu66zHTkCB3WxLctAh7m0kw==";
+    const mastKey = "cWFk3AhIa0nW2odlWhFskJsPDBlsuScVn8UgUyStIzggXK36exGn3umMZ2sjTXKXfzrXy78tC2na2sNRZS873Q==";
     const today = new Date();
     const UTCstring = today.toUTCString();
 
